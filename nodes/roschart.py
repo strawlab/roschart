5752cdeb678e07364c32c0e07ea1b833701259ea
John Stowers <john.stowers@gmail.com>
2012-06-18 12:21:52 +0200

    Fix the ROS/Gtk threading assumptions
    The ROS threading model requires calling init_node from the
    main thread (because it may setup signal handlers).
    Each subscriber (and publisher, not used here)
    is assigned a thread and callbacks happen in that thread.
    It is not even necessary to call rospy.spin() in the subscriber
    case (with signals disabled) however one should call rospy.spin
    for future compatibility. The conclusion of this is that one should
     * call init_node from the main thread
     * feel free to call rospy.spin in a non-main thread
     * remember data callbacks occur in non-main threads
    
    Gtk is thread-aware but not thread safe. You can not update
    UI state from non-main threads, but Gtk programs can use threading
    providing the appropriate thread_init functions are called before
    constructing anything. This means that if you recieve data in a non-main
    thread an wish to update the UI there are two recommended patterns
     * use the GLib.idle_add API to schedule a function to be called from the
       main (gtk thread)
     * take the gtk lock (not recommneded because of problems on windows)
     * Use GObject signals (which are list of function callbacks) and 'emit'
       those signals on idle - this is essentially the first option, but fits
       more with the common model/view+controller split of Gtk applications
     * serialise UI updates to occur at regular intervals from the main thread
    
    I took the last approach because I expect data to be arriving faster than
    we can show it (i.e. sync to vblank limits the max framerate to 60Hz or so,
    and we really can see much benefit in FPS > 30 anyway).
    
    This means that I
     * schedule a timer to fire at the interval I want to update the UI
     * get data in a random subscriber thread, take a lock, and store it
     * get the timer callback, take the lock and update the UI

--- nodes/roschart.py
+++ nodes/roschart.py
@@ -1,83 +1,53 @@
 #!/usr/bin/env python
 
-import multiprocessing
-import os
-import Queue
-import signal
+import threading
 
 import roslib; roslib.load_manifest( 'roschart' )
 import rospy
 
 from roschart.msg import Point
 
-from gi.repository import Gtk, GObject
+from gi.repository import Gtk, Gdk, GObject, GLib
 
-class Listener (multiprocessing.Process):
-    """Wait for rospy "roschart" messages."""
-    def __init__( self ):
-        multiprocessing.Process.__init__( self )
-        self.queue = multiprocessing.Queue()
-
-    def run( self ):
-        rospy.init_node( 'roschart', anonymous=True )
-        rospy.Subscriber( 'roschart', Point, self.data_callback )
-        rospy.spin()
-
-    def data_callback( self, point ):
-        self.queue.put( point )
-
+GObject.threads_init()
+Gdk.threads_init()
 
 class Plotter (Gtk.Window):
+    UPDATE_FREQ = 30
     """Open a GTK window and respond to rospy input events."""
     def __init__( self ):
         Gtk.Window.__init__( self, title="roschart" )
 
         self.label = Gtk.Label( label="waiting for data..." )
         self.add( self.label )
-        
-        self.listener = Listener()
-        self.listener.start()
+        self.connect( "delete-event", Gtk.main_quit )
 
-        self.connect( "delete-event", self.quit )
+        self.point = Point()
+        self.lock = threading.Lock()
+        self.sub = rospy.Subscriber( 'roschart', Point, self.data_callback )
         
         self.show_all()
 
+    def data_callback(self, msg):
+        with self.lock:
+            self.point = msg
+
+    def update_ui(self):
+        with self.lock:
+            self.label.set_text(str(self.point))
+        return True
+
     def start( self ):
-        self.alive = True
-        self.poll_queue()
+        rosthread = threading.Thread(name="ros spin thread", target=rospy.spin)
+        rosthread.daemon = True
+        rosthread.start()
+        GLib.timeout_add(1000/self.UPDATE_FREQ, self.update_ui)
+        Gtk.main()
 
-    def poll_queue( self ):
-        while self.alive:
-            try:
-                data = self.listener.queue.get( timeout=0.005 )
-            except Queue.Empty:
-                pass
-            else:
-                self.label.set_text( str( data ) )
-
-            while Gtk.events_pending():
-                Gtk.main_iteration()
-
-    def quit( self, obj, data ):
-        """Send an interrupt (Ctrl+C) to listener; wait for rospy shutdown."""
-        self.alive = False
-        
-        os.kill( self.listener.pid, signal.SIGINT )
-        self.listener.join()
-# for Windows, where SIGINT is reputed not to exist:
-# http://objectmix.com/python/387639-sending-cntrl-c.html
-##To the OP: you can download the pywin32 package from sourceforge, and use
-##win32api.GenerateConsoleCtrlEvent(win32con.CTRL_C_EVENT, pgid)
-##or call the same function using ctypes.
-##See http://msdn.microsoft.com/en-us/library/ms683155(VS.85).aspx for some
-##important remarks.
-
-        Gtk.main_quit()
-        
 
 if __name__ == '__main__':
+    rospy.init_node( 'roschart', anonymous=True, disable_signals=True )
     plotter = Plotter()
     plotter.start()
+    rospy.signal_shutdown('quit from gui')
 
-    # pressing Ctrl+C before closing the window hangs the main process,
-    # but this appears to be the fault of this GTK setup, independent of ROS
